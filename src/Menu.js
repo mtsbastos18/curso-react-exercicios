18a30d2d1fca95c91a63c4c0d592b7dd50d5e4df
Mateus Bastos <mtsbastos18@gmail.com>
2019-11-05 13:01:30 -0300

    a

--- src/Menu.js
+++ src/Menu.js
@@ -4,8 +4,20 @@ import { createDrawerNavigator } from 'react-navigation'
 import Simples from './componentes/Simples';
 import ParImpar from './componentes/ParImpar';
 import { Inverter, MegaSena } from './componentes/Multi';
+import Contador from './componentes/Contador';
+import Plataforma from './componentes/Plataforma';
+import ValidarProps from './componentes/ValidarProps';
 
 export default createDrawerNavigator({
+    ValidarProps: {
+        screen: () => <ValidarProps label="Ano: " ano={18} />
+    },
+    Plataforma: {
+        screen: () => <Plataforma/>
+    },
+    Contador: {
+        screen: () => <Contador />
+    },
     MegaSena: {
         screen: () => <MegaSena numeros={6} />,
         navigationOptions: { title: 'Mega Sena' }
